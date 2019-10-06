73b779893ae8b0c0e096cf5aabdb75c7bc3dcdbf
Henry Post <HenryFBP@gmail.com>
2019-10-05 23:14:03 -0500

    almost finish lab 3

---
+++ labs/03/lab3-queries.sql
@@ -0,0 +1,13 @@
+USE `mydb` ;
+
+-- Henry Post, hpost@hawk.iit.edu
+-- ITMD422 at IIT
+
+-- Hey TAs. These queries are blank.
+-- I did this at the last minute unfortunately.
+
+-- Running Linux and used MySQL Workbench to generate a Chen diagram.
+-- Exported this diagram to an SQL script.
+
+-- I am running MySQL 14.14 Distrib 5.7.27 and the generated SQL scripts are not compatible.
+-- I don't have enough time to re-do these CREATE TABLE statements, so there are no queries. Sorry about that.
---
+++ labs/03/make-pdfs.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+enscript -Esql --color=yes -o lab3-queries.out.pdf lab3-queries.sql
+
+enscript -Esql --color=yes -o lab3-tables.out.pdf lab3-tables.sql
