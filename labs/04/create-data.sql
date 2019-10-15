f9ecd6d9230094d7faa3f8e7d6dfe2f6b5f0e352
Henry Post <HenryFBP@gmail.com>
2019-10-14 20:50:54 -0500

    create data

---
+++ labs/04/create-data.sql
@@ -0,0 +1,77 @@
+
+DROP SCHEMA IF EXISTS `mydb`;
+CREATE DATABASE IF NOT EXISTS `mydb`;
+USE `mydb`;
+
+CREATE TABLE IF NOT EXISTS `PersonType` (
+    `typeID` INT PRIMARY KEY NOT NULL,
+    `typeName` VARCHAR(100) NOT NULL
+)  COMMENT='
+A type of person, i.e. \'Manager\', \'Employee\', \'Intern\'.
+';
+
+CREATE TABLE IF NOT EXISTS `LocationType` (
+    `typeID` INT PRIMARY KEY NOT NULL,
+    `typeName` VARCHAR(100) NOT NULL
+)  COMMENT='
+A specific location type, i.e. \'Executive Washroom\', \'Lobby\', \'Server Room\'.
+';
+
+CREATE TABLE IF NOT EXISTS `Employee` (
+    `employeeID` INT PRIMARY KEY NOT NULL,
+    `lastName` VARCHAR(100),
+    `dateJoined` DATE,
+    `personTypeID` INT,
+    FOREIGN KEY (`personTypeID`)
+        REFERENCES `PersonType` (`typeID`)
+)  COMMENT='
+asdf
+';
+
+CREATE TABLE IF NOT EXISTS `PersonLocationAccess` (
+    `personTypeID` INT NOT NULL,
+    `locationTypeID` INT NOT NULL,
+    FOREIGN KEY (`personTypeID`)
+        REFERENCES `PersonType` (`typeID`),
+    FOREIGN KEY (`locationTypeID`)
+        REFERENCES `LocationType` (`typeID`)
+)  COMMENT='
+This table links together the type of a person and the type of location they have access to.
+The existence of a person\'s ID with a location type in this table means that type of person has access to that type of location.
+';
+
+INSERT INTO `PersonType` VALUES
+	(0,'Managers'),
+	(1,'Administrative Assistants'),
+	(2,'Information Technology Employees'),
+	(3,'Clerks'),
+	(4,'Collegiate Interns'),
+	(5,'Maintenance Crew'),
+	(6,'Visitors');
+
+INSERT INTO `LocationType` VALUES
+	(0,	'Executive Lounge'),
+	(1,	'Executive Washroom'),
+	(2,	'Supply Closet'),
+	(3,	'Employee Lounge'),
+	(4,	'Server Room');
+
+INSERT INTO `PersonLocationAccess` VALUES
+  (0, 0),
+  (0, 1),
+  (1, 0),
+  (1, 2),
+  (1, 3),
+  (2, 3),
+  (2, 4),
+  (3, 2),
+  (3, 3),
+  (4, 3),
+  (5, 0),
+  (5, 1),
+  (5, 2),
+  (5, 3),
+  (5, 4),
+  (6, 0),
+  (6, 1),
+  (6, 3);
